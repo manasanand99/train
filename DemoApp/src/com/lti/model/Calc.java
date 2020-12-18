d71d7c3491cccbdf38c302a6e0fe83395854b496
Hriday <Hriday@DESKTOP-QCSDHJB>
2020-12-18 16:41:51 +0530

    added sub

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,5 +5,7 @@ public class Calc {
 	public void add(int n1,int n2) {
 		System.out.println("sum = "+n1+n2);
 	}
-	
+	public void sub(int n1,int n2) {
+		System.out.println("diff = "+(n1-n2));
+	}
 }
