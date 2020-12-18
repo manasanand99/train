abf0242af90bd6890c68a90a7abb436430c8f2b9
Hriday <Hriday@DESKTOP-QCSDHJB>
2020-12-18 16:07:31 +0530

    added

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Sample {
+
+	public  void show() {
+		// TODO Auto-generated method stub
+
+		System.out.println("hellow world");
+	}
+
+	public void method() {
+		System.out.println("hi ,alll");
+	}
+}
